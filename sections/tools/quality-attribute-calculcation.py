3e56a1ee78bd182de9777192ce69249954854a69
frithjofhoppebit <frithjof.hoppe@bit.admin.ch>
2026-03-13 15:37:18 +0100

    feat: :sparkles: Evaluate variant 1

--- sections/tools/quality-attribute-calculcation.py
+++ sections/tools/quality-attribute-calculcation.py
@@ -6,13 +6,13 @@
 
 quality_attributes = {
     "Performance": (0.3, 2),
-    "Portability": (1, 3),
-    "Auditability": (0.6, 3),
+    "Portability": (1, 4),
+    "Auditability": (0.6, 4),
     "Automatibility": (1, 3),
     "Maintainability": (0.8, 3),
-    "Adaptability": (0.6, 2),
-    "Complexity": (0.8, 3),
-    "Traceability Strength": (1, 4),
+    "Adaptability": (0.6, 3),
+    "Complexity": (0.8, 4),
+    "Traceability Strength": (1, 2),
 }
 
 weighted_sum = 0
